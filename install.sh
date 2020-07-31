fbcc32d8868c9bd2be9fcc3a36a3483998f500df
Dean Keeble <dean.keeble@diamond.ac.uk>
2020-07-31 16:01:20 +0100

    added old .vimrc checking and removal

--- install.sh
+++ install.sh
@@ -3,7 +3,20 @@
 echo "Don't forget to install ripgrep if you don't have it already"
 echo "$ sudo apt-get install ripgrep"
 echo "$ or google it"
+
 # make undotree directory
 mkdir -p ~/.vim/undotree
+
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# if there's a .vimrc already, tell user and abort unless -f flag found
+if [ -f ~/.vimrc ]; then
+    if [ "$1" == "-f" ]; then
+        echo "Removing old .vimrc"
+        rm -rf ~/.vimrc;
+    else
+        echo "there is already a .vimrc. add -f to force the issue"
+        exit 0
+    fi
+fi
 ln -s "$SCRIPTPATH/vimrc" ~/.vimrc
