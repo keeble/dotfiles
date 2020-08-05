60c47c3af3e526f9c94e309ffbc8a62a011bbb5f
Dean Keeble <dean.keeble@diamond.ac.uk>
2020-08-05 17:39:55 +0100

    simpler way to deal with bashrc

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+echo ""
 echo "Don't forget to install ripgrep if you don't have it already"
 echo "$ sudo apt-get install ripgrep"
 echo "$ or google it"
+echo ""
 
 # make undotree directory
 mkdir -p ~/.vim/undotree
@@ -12,13 +13,15 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 # if there's a .vimrc already, tell user and abort unless -f flag found
 if [ -f ~/.vimrc ] || [ -f ~/.bashrc_local ]; then
     if [ "$1" == "-f" ]; then
-        echo "Removing old .vimrc and/or .bashrc_local"
+        echo "Removing old .vimrc "
         rm -rf ~/.vimrc;
-        rm -rf ~/.bashrc_local;
     else
-        echo "there is already a .vimrc and/or .bashrc_local add -f to force the issue"
+        echo "there is already a .vimrc. add -f to force the issue"
         exit 0
     fi
 fi
 ln -s "$SCRIPTPATH/vimrc" ~/.vimrc
-ln -s "$SCRIPTPATH/bashrc_local" ~/.bashrc_local
+echo ""
+echo "ln -s "$SCRIPTPATH/bashrc_local" ~/.bashrc_local"
+echo "... to also link the bashrc"
+
