d8b58476b984d7a524ba3d9a9076b25ca0c0592c
Dean Keeble <dean.keeble@diamond.ac.uk>
2020-08-05 17:31:18 +0100

    added bashrc_local and misc DLS functions

--- install.sh
+++ install.sh
@@ -10,13 +10,15 @@ mkdir -p ~/.vim/undotree
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 # if there's a .vimrc already, tell user and abort unless -f flag found
-if [ -f ~/.vimrc ]; then
+if [ -f ~/.vimrc ] || [ -f ~/.bashrc_local ]; then
     if [ "$1" == "-f" ]; then
-        echo "Removing old .vimrc"
+        echo "Removing old .vimrc and/or .bashrc_local"
         rm -rf ~/.vimrc;
+        rm -rf ~/.bashrc_local;
     else
-        echo "there is already a .vimrc. add -f to force the issue"
+        echo "there is already a .vimrc and/or .bashrc_local add -f to force the issue"
         exit 0
     fi
 fi
 ln -s "$SCRIPTPATH/vimrc" ~/.vimrc
+ln -s "$SCRIPTPATH/bashrc_local" ~/.bashrc_local
